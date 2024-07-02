db764efa76e68e9e3388a96694d5b6cdc956ee84
Aktivv <e.n.d.kgkg@gmail.com>
2024-07-02 20:56:05 +0600

    first lesson

---
+++ main.py
@@ -0,0 +1,16 @@
+from config import dp
+from aiogram.utils import executor
+import logging
+from handlers import commands, echo
+
+commands.register_commands(dp)
+
+
+
+# Эхо функция - вызывать самым последним
+echo.register_echo(dp)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp, skip_updates=True)
