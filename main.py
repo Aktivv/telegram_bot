1642add89706791c98b80108002e40f8397441a3
Aktivv <e.n.d.kgkg@gmail.com>
2024-07-18 22:36:07 +0600

    second_ls

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from config import dp
+from config import dp, Admin, bot
 from aiogram.utils import executor
 import logging
-from handlers import commands, echo
+from handlers import commands, echo, quiz
+
+async def on_startup(_):
+    for i in Admin:
+        await bot.send_message(chat_id=i, text='Bot started')
+
+async def on_shutdown(_):
+    for i in Admin:
+        await bot.send_message(chat_id=i, text='Bot stopped')
+
 
 commands.register_commands(dp)
-
+quiz.register_quiz(dp)
 
 
 # Эхо функция - вызывать самым последним
@@ -13,4 +22,4 @@ echo.register_echo(dp)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
